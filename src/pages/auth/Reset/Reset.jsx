daf407ab45bef834e8d7e649552f006d1e3dc853
Gobinda Saha <72671943+gtechgobinda@users.noreply.github.com>
2023-05-23 18:23:41 +0530

    #6 completd

--- src/pages/auth/Reset/Reset.jsx
+++ src/pages/auth/Reset/Reset.jsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Reset.scss";
 const Reset = () => {
+  const [email, setEmail] = useState("");
+  const navigate = useNavigate();
+
   return (
     <>
-      <div>Reset</div>
+      <div className="reset-form">
+        <div className="form-content">
+          <header>Reset Password</header>
+          <form className="form">
+            <div className="field input-field">
+              <input
+                type="email"
+                placeholder="Enter Email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                required
+              />
+            </div>
+            <div className="field button-field">
+              <button>Reset Password</button>
+            </div>
+          </form>
+          <div className="form-link">
+            <span onClick={() => navigate("/login")}>-Login</span>
+            <span onClick={() => navigate("/register")}>-Register</span>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
