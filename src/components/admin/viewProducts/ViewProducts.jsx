dc3964bf7a0514e319376232fe135543ddaede55
Gobinda Saha <72671943+gtechgobinda@users.noreply.github.com>
2023-06-11 08:50:45 +0530

    #24 completed

--- src/components/admin/viewProducts/ViewProducts.jsx
+++ src/components/admin/viewProducts/ViewProducts.jsx
@@ -1,10 +1,20 @@
-import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
+import Notiflix from "notiflix";
 import { useEffect, useState } from "react";
 import { BiEdit } from "react-icons/bi";
 import { MdDeleteForever } from "react-icons/md";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
-import { db } from "../../../firebase/config.js";
+import { db, storage } from "../../../firebase/config.js";
+import { Loader } from "../../index.js";
 import "./ViewProducts.scss";
 const ViewProducts = () => {
   const [products, setProducts] = useState([]);
@@ -31,10 +41,48 @@ const ViewProducts = () => {
     } catch (error) {
       setIsLoading(false);
       toast.error(error.message);
+      console.error(error);
     }
   };
+
+  const confirmDelete = (id, imageURL) => {
+    Notiflix.Confirm.show(
+      "Delete Product!!!",
+      "You are about delete this product?",
+      "Delete",
+      "Cancel",
+      function okCb() {
+        deleteProduct(id, imageURL);
+      },
+      function cancelCb() {
+        console.log("Delete Canceled");
+      },
+      {
+        width: "320px",
+        borderRadius: "3px",
+        titleColor: "#8e2de2",
+        okButtonBackground: "#8e2de2",
+        cssAnimationStyle: "zoom",
+      }
+    );
+  };
+
+  const deleteProduct = async (id, imageURL) => {
+    try {
+      await deleteDoc(doc(db, "products", id));
+
+      const storageRef = ref(storage, imageURL);
+      await deleteObject(storageRef);
+      toast.success("Product Deleted Successfully");
+    } catch (error) {
+      toast.error(error.message);
+      console.error(error);
+    }
+  };
+
   return (
     <>
+      {isLoading && <Loader />}
       <div className="table">
         <h2>All Products</h2>
         {products.length === 0 ? (
@@ -68,12 +116,16 @@ const ViewProducts = () => {
                       <td>{name}</td>
                       <td>{category}</td>
                       <td>{`₹${price}`}</td>
-                      <td>
+                      <td className="icons">
                         <Link to="/admin/add-product">
                           <BiEdit size={20} color="green" />
                         </Link>
                         &nbsp;
-                        <MdDeleteForever size={20} color="red" />
+                        <MdDeleteForever
+                          size={20}
+                          color="red"
+                          onClick={() => confirmDelete(id, imageURL)}
+                        />
                       </td>
                     </tr>
                   </>
