5de0fc705b7cfdf77dc14f3640df3fc93a183e33
Gobinda Saha <72671943+gtechgobinda@users.noreply.github.com>
2023-06-13 22:07:51 +0530

    #28 completed

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,9 +1,27 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  STORE_PRODUCTS,
+  selectProducts,
+} from "../../redux/slice/productSlice.jsx";
+import useFetchCollection from "../customHooks/useFetchCollection.jsx";
 import { ProductList } from "../index.js";
 import "./Product.scss";
 const Product = () => {
+  const { data, isLoading } = useFetchCollection("products");
+  const products = useSelector(selectProducts);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(
+      STORE_PRODUCTS({
+        products: data,
+      })
+    );
+  }, [dispatch, data]);
   return (
     <>
-      <ProductList />
+      <ProductList products={products} />
     </>
   );
 };
