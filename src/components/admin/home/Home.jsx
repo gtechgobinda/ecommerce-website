0c9d9030f6706b02a5d4cc7c9afaf6a67a1a3b94
Gobinda Saha <72671943+gtechgobinda@users.noreply.github.com>
2023-06-27 19:16:09 +0530

    #63 completed

--- src/components/admin/home/Home.jsx
+++ src/components/admin/home/Home.jsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
 import { BiRupee } from "react-icons/bi";
 import { FaCartArrowDown } from "react-icons/fa";
 import { GrCart } from "react-icons/gr";
+import { useDispatch, useSelector } from "react-redux";
+import useFetchCollection from "../../../customHooks/useFetchCollection";
+import {
+  CALC_TOTAL_ORDER_AMOUNT,
+  STORE_ORDERS,
+  selectOrderHistory,
+  selectTotalOrderAmount,
+} from "../../../redux/slice/orderSlice";
+import {
+  STORE_PRODUCTS,
+  selectProducts,
+} from "../../../redux/slice/productSlice";
 import InfoBox from "../../infoBox/InfoBox";
 import "./Home.scss";
 
@@ -9,17 +22,41 @@ const earningIcon = <BiRupee color="black" size={25} />;
 const productIcon = <GrCart color="black" size={25} />;
 const ordersIcon = <FaCartArrowDown color="black" size={25} />;
 const Home = () => {
+  const products = useSelector(selectProducts);
+  const orders = useSelector(selectOrderHistory);
+  const totalOrderAmount = useSelector(selectTotalOrderAmount);
+
+  const fbProducts = useFetchCollection("products");
+  const { data } = useFetchCollection("orders");
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(
+      STORE_PRODUCTS({
+        products: fbProducts.data,
+      })
+    );
+    dispatch(STORE_ORDERS(data));
+    dispatch(CALC_TOTAL_ORDER_AMOUNT());
+  }, [dispatch, data, fbProducts]);
   return (
     <>
       <div className="admin-home-container">
         <div className="info-box1">
-          <InfoBox title={"Earning"} count={`₹50000`} icon={earningIcon} />
+          <InfoBox
+            title={"Earning"}
+            count={`₹${totalOrderAmount}`}
+            icon={earningIcon}
+          />
         </div>
         <div className="info-box2">
-          <InfoBox title={"Products"} count={`188`} icon={productIcon} />
+          <InfoBox
+            title={"Products"}
+            count={products.length}
+            icon={productIcon}
+          />
         </div>
         <div className="info-box3">
-          <InfoBox title={"Orders"} count={`18`} icon={ordersIcon} />
+          <InfoBox title={"Orders"} count={orders.length} icon={ordersIcon} />
         </div>
       </div>
     </>
