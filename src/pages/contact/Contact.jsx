d0003b3349a0c20a1d5e8df2dadcff11e29e46e6
Gobinda Saha <72671943+gtechgobinda@users.noreply.github.com>
2023-06-29 09:09:51 +0530

    #70-126 completed

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,12 +1,34 @@
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
+import { toast } from "react-toastify";
 import "./Contact.scss";
 const Contact = () => {
-  const sendEmail = () => {};
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_6amjava",
+        "template_unx8tri",
+        form.current,
+        "jW47gE_rb8WNC-23d"
+      )
+      .then(
+        (result) => {
+          toast.success("Message Sent Successfully");
+        },
+        (error) => {
+          toast.error(error.text);
+        }
+      );
+  };
   return (
     <>
       <div className="contact-form">
         <div className="form-content">
           <h2 className="contact-form-heading">Contact US</h2>
-          <form onSubmit={sendEmail} className="form">
+          <form ref={form} onSubmit={sendEmail} className="form">
             <div className="field input-field">
               <input
                 type="text"
