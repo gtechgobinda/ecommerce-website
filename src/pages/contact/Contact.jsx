87d9cdbda5ebdd8ee53b8c4452949a49ae60dbc1
Gobinda Saha <72671943+gtechgobinda@users.noreply.github.com>
2023-06-29 08:17:35 +0530

    #70-125 completed

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,45 @@
 import "./Contact.scss";
 const Contact = () => {
+  const sendEmail = () => {};
   return (
     <>
-      <div>Contact</div>
+      <div className="contact-form">
+        <div className="form-content">
+          <h2 className="contact-form-heading">Contact US</h2>
+          <form onSubmit={sendEmail} className="form">
+            <div className="field input-field">
+              <input
+                type="text"
+                name="user_name"
+                placeholder="Full Name"
+                required
+              />
+            </div>
+            <div className="field input-field">
+              <input
+                type="email"
+                name="user_email"
+                placeholder="Your active email"
+                required
+              />
+            </div>
+            <div className="field input-field">
+              <input
+                type="text"
+                name="subject"
+                placeholder="Subject"
+                required
+              />
+            </div>
+            <div className="field area-field">
+              <textarea name="message" cols="30" rows="10"></textarea>
+            </div>
+            <div className="field button-field">
+              <button className="send-msg-btn">Send Message</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
